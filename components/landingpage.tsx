2521f873a495d9fa7de415e0b79b7a3cbb294d7c
dvip1 <dvipatwork@outlook.com>
2025-02-02 23:53:28 +0530

    add descr

--- components/landingpage.tsx
+++ components/landingpage.tsx
@@ -11,6 +11,7 @@ export default function LandingPage() {
         <div className={`text-xl min-h-screen flex justify-center items-center bg-[#BFECFF]`}>
             <div className="banner ">
                 <p className={`text-5xl font-bold ${getQuickSand.className}`}>Learn Python!</p>
+                <p className='text-sm mt-2  text-gray-500'>Description: A python tutor that learns about the user and teaches them based on that information</p>
                 <button onClick={handleClick} className='bg-[#E0CD5E] p-3 rounded-xl mt-5 border-solid border-2 border-slate-500'>get started!</button>
             </div>
         </div>
